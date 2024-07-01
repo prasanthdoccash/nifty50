9402f6922b3cedd9c7ed20c493761212a9025956
prasanthdoccash <64533752+prasanthdoccash@users.noreply.github.com>
2024-07-01 12:35:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -321,4 +321,4 @@ def delivery():
     return render_template('intraday_analysis.html', results=results, last_refreshed=last_refreshed)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
